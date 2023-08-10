b7d00bf2f55ffdde94e26b2d1598ffc7a5f29c41
HemantJ07 <87138505+HemantJ07@users.noreply.github.com>
2023-08-10 11:55:47 +0530

    Update arthmetic_arranger.py

--- arthmetic_arranger.py
+++ arthmetic_arranger.py
@@ -1,58 +1,64 @@
-import re 
-
-def arthmetic_arranger(problems):
-  if (len(problems)>5):
-    return "Error: Too many problems."
-  first=""
-  second=""
-  lines=""
-  sumx=""
-  string=""
-  for problem in problems:
-    if(re.search("[^\s0-9.+-",problem)):
-      if(re.search("[/]",problem) or re.search("[*]",problem)):
-        
-        return "Error: Operator must be '+' or '-'."
-    return "Error: Numbers must only contain digits."
-
-  firstNumber= problem.split(" ")[0]
-  operator= problem.split(" ")[1]
-  secondNumber = problem.split(" ")[2]
-
-  if(len(firstNumber) >=5 or len(secondNumber) >=5  ):
-    return "Error: Numbers cannot be more than four digits."
-
-  sum=""
-  if(operator=="+"):
-    sum=str(int(firstNumber) + int(secondNumber))
-  elif(operator=="-"):
-    sum=str(int(firstNumber) - int(secondNumber))
-
-  length=max(len(firstNumber),len(secondNumber))
-  top = str(firstNumber).rjust(length)
-  bottom= operator + str(secondNumber).rjust(length-1)
-  line=""
-  res = str(sum).rjust(length)
-  for s in range (length):
-    line+="-"
-  if problem !=problems[-1]:
-    first+= top + '   '
-    second+= bottom+ '   '
-    lines+= line + '   '
-    sumx += res+ '   '
-  else:
-    first+= top 
-    second+= bottom
-    lines+= line
-    sumx += res
-
-  if solve:
-    string = first + "\n" + second +"\n"+ lines +"\n"+sumx
-  else:
-    string = first + "\n" + second +"\n"+ lines
-  return string
+def arithmetic_arranger(problems, answer=False):
+    # Check the number of problems
+    if len(problems) > 5:
+        return "Error: Too many problems."
     
-  
-      
+    first_operand = []
+    second_operand = []
+    operator = []
 
-  return arranged_problems 
+    for problem in problems:
+        pieces = problem.split()
+        first_operand.append(pieces[0])
+        operator.append(pieces[1])
+        second_operand.append(pieces[2])
+
+    # Check for * or /
+    if "*" in operator or "/" in operator:
+        return "Error: Operator must be '+' or '-'."
+
+    # Check the digits
+    for i in range(len(first_operand)):
+        if not (first_operand[i].isdigit() and second_operand[i].isdigit()):
+            return "Error: Numbers must only contain digits."
+
+    # Check the length
+    for i in range(len(first_operand)):
+        if len(first_operand[i]) > 4 or len(second_operand[i]) > 4:
+            return "Error: Numbers cannot be more than four digits."
+
+    first_line = []
+    second_line = []
+    third_line = []
+    fourth_line = []
+
+    for i in range(len(first_operand)):
+        if len(first_operand[i]) > len(second_operand[i]):
+            first_line.append(" "*2 + first_operand[i])
+        else:
+            first_line.append(" "*(len(second_operand[i]) - len(first_operand[i]) + 2) + first_operand[i])
+
+    for i in range(len(second_operand)):
+        if len(second_operand[i]) > len(first_operand[i]):
+            second_line.append(operator[i] + " " + second_operand[i])
+        else:
+            second_line.append(operator[i] + " "*(len(first_operand[i]) - len(second_operand[i]) + 1) + second_operand[i])
+
+    for i in range(len(first_operand)):
+        third_line.append("-"*(max(len(first_operand[i]), len(second_operand[i])) + 2))
+
+    if answer:
+        for i in range(len(first_operand)):
+            if operator[i] == "+":
+                ans = str(int(first_operand[i]) + int(second_operand[i]))
+            else:
+                ans = str(int(first_operand[i]) - int(second_operand[i]))
+
+            if len(ans) > max(len(first_operand[i]), len(second_operand[i])):
+                fourth_line.append(" " + ans)
+            else:
+                fourth_line.append(" "*(max(len(first_operand[i]), len(second_operand[i])) - len(ans) + 2) + ans)
+        arranged_problems = "    ".join(first_line) + "\n" + "    ".join(second_line) + "\n" + "    ".join(third_line) + "\n" + "    ".join(fourth_line)
+    else:
+        arranged_problems = "    ".join(first_line) + "\n" + "    ".join(second_line) + "\n" + "    ".join(third_line)
+    return arranged_problems
